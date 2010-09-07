a5c9eb2242cde33427623b28829b3166b44e5227
Mark Aufdencamp <Mark@Aufdencamp.com>
2010-09-07 16:56:14 -0400

    A couple initial Google methods

--- app/controllers/google_controller.rb
+++ app/controllers/google_controller.rb
@@ -1,5 +1,19 @@
+require 'oauth'
+require 'oauth/consumer'
+require 'yaml'
+require 'json'
+
 class GoogleController < ApplicationController
   
   layout "service"
+  
+  def authorizeGoogleAccess
+    
+  end
+  
+  def retrieveGoogleContacts
+    
+  end
+  
   
 end
