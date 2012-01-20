35101a02911ed2fd51e186300e002aac5b98d102
Mark Aufdencamp <Mark@Aufdencamp.com>
2012-01-20 16:08:27 -0500

    Initial methods and documentation locations to get started on Windows Live Integration

--- app/controllers/windowslive_controller.rb
+++ app/controllers/windowslive_controller.rb
@@ -2,4 +2,27 @@ class WindowsliveController < ApplicationController
 
   layout "service"
 
+# http://msdn.microsoft.com/en-us/windowslive/
+# Learn Live Connect
+# http://msdn.microsoft.com/en-us/windowslive/ff621314
+# Identity (profiles)
+# http://msdn.microsoft.com/en-us/windowslive/hh278356
+# Hotmail (contacts and calendars)
+# http://msdn.microsoft.com/en-us/windowslive/hh528486
+# Scopes and Permissions
+# http://msdn.microsoft.com/en-us/library/hh243646.aspx
+# Obtaining user consent
+# http://msdn.microsoft.com/en-us/windowslive/hh278359
+
+  def authorizeWindowsLiveAccess
+    
+  end
+  
+  def retrieveHotmailContacts
+    
+  end
+  
+private
+
+
 end
