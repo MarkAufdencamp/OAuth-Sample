711c1d5e64efdf217dd80fb9ff138cc212ae286f
Mark Aufdencamp <Mark@Aufdencamp.com>
2012-01-20 16:02:27 -0500

    Define  a couple methods and some documentation locations to get started on AOL integration

--- app/controllers/aol_controller.rb
+++ app/controllers/aol_controller.rb
@@ -2,4 +2,18 @@ class AolController < ApplicationController
   
   layout "service"
   
+# http://dev.aol.com/
+# http://dev.aol.com/oauth
+# http://dev.aol.com/api/openmail
+
+  def authorizeAOLAccess
+    
+  end
+  
+  def retrieveAOLContacts
+    
+  end
+  
+private
+
 end
