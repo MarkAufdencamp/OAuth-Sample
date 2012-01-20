1ba2d2c5818a85e45617ca8df40a9ad2b9fe5d79
Mark Aufdencamp <Mark@Aufdencamp.com>
2012-01-20 13:23:10 -0500

    Refactored to retrieve Google Profile and pass Google Name to View

--- app/controllers/google_controller.rb
+++ app/controllers/google_controller.rb
@@ -5,6 +5,11 @@ class GoogleController < ApplicationController
 # http://code.google.com/apis/contacts
 # http://code.google.com/apis/contactsdocs/3.0/developers_guide_protocol.html
 # http://code.google.com/apis/gdata/articles/using_ruby.html
+# http://code.google.com/apis/accounts/docs/OAuth.html
+# http://code.google.com/apis/accounts/docs/OAuth2.html
+# http://code.google.com/apis/gdata/articles/oauth.html
+# http://code.google.com/apis/gdata/faq.html#AuthScopes
+# http://code.google.com/apis/gdata/docs/auth/oauth.html#Scope
 
   def authorizeGoogleAccess
     # Retrieve Request Token from Google and Re-Direct to Google for Authentication
@@ -13,8 +18,9 @@ class GoogleController < ApplicationController
     #logger.info 'Consumer Key - ' + credentials['Consumer Key']
     #logger.info 'Consumer Secret - ' + credentials['Consumer Secret']
     auth_consumer = getAuthConsumer credentials
-                  
-    request_token = auth_consumer.get_request_token({:oauth_callback => credentials['Callback URL'] }, {:scope => 'https://www.google.com/m8/feeds/'})
+    
+    auth_scope = 'https://www.googleapis.com/auth/userinfo.profile https://www.google.com/m8/feeds/'
+    request_token = auth_consumer.get_request_token({:oauth_callback => credentials['Callback URL'] }, {:scope => auth_scope})
     if request_token.callback_confirmed?
       #Store Token and Secret to Session
       session[:request_token] = request_token.token
@@ -62,33 +68,19 @@ class GoogleController < ApplicationController
     end
     
     # Retrieve Google GUID and Contacts
-    @guid = ''
-    @contacts = []
+    @googleId = ''
+    @googleName = ''
+    @googleContacts = []
     if got_request_token and got_access_token
-      response = getGoogleContacts access_token
-      #logger.info response
-      # Convert the XML Response to a Hash
-      result = XmlSimple::xml_in(response)
-      #PP::pp result, $stderr, 50
-      @guid = result["id"]
-      @contacts = parseContactsResponse result
+      userProfile = getGoogleProfile access_token
+      @googleId = userProfile['id']
+      @googleName = userProfile['name']
+      @googleContacts = getGoogleContacts access_token
     end
 
   end
 
 private
-
-  def getGoogleContacts access_token
-    # Google URL for Full Contact Retrieval
-    #   "http://www.google.com/m8/feeds/contacts/default/full"
-    # Alternately the Google userId can be substituted for default
-    #   "http://www.google.com/m8/feeds/contacts/hostmaster@iluviya.net/full"
-    # for JSON instead of XML contacts_url = "/m8/feeds/contacts/default/full?alt=json"
-    contacts_url = "/m8/feeds/contacts/default/full"
-    response = access_token.get(contacts_url)
-    data = response.body
-  end
-
 
   def getAuthConsumer credentials
     OAuth::Consumer.new(credentials['Consumer Key'],
@@ -102,6 +94,31 @@ private
         })        
   end
   
+  def getGoogleProfile access_token
+ 
+    profile_url = "https://www.googleapis.com/oauth2/v1/userinfo"
+    response = access_token.get(profile_url)
+    result = response.body
+    data = JSON.parse(result)
+    
+  end
+
+  def getGoogleContacts access_token
+    # Google URL for Full Contact Retrieval
+    #   "http://www.google.com/m8/feeds/contacts/default/full"
+    # Alternately the Google userId can be substituted for default
+    #   "http://www.google.com/m8/feeds/contacts/hostmaster@iluviya.net/full"
+    # for JSON instead of XML contacts_url = "/m8/feeds/contacts/default/full?alt=json"
+    contacts_url = "/m8/feeds/contacts/default/full"
+    response = access_token.get(contacts_url)
+    result = response.body
+    # Convert the XML Response to a Hash
+    data = XmlSimple::xml_in(result)
+    #PP::pp data, $stderr, 50
+    googleContacts = parseContactsResponse data
+    
+  end
+
 
   def parseContactsResponse doc
 
