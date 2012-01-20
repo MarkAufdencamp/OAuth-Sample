cd52dfa97ece5cb2bc4e60f7f3a4292c6c82afcd
Mark Aufdencamp <Mark@Aufdencamp.com>
2012-01-20 16:07:46 -0500

    Initialize methods and documentation locations to get started on MySpace integration

--- app/controllers/myspace_controller.rb
+++ app/controllers/myspace_controller.rb
@@ -1,5 +1,18 @@
 class MyspaceController < ApplicationController
   
   layout "service"
+
+# http://developer.myspace.com/modules/apps/pages/myapps.aspx
+# http://wiki.developer.myspace.com/index.php?title=Main_Page
+# http://wiki.developer.myspace.com/index.php?title=OAuth_REST_API_Usage_-_Authentication_Process  
+  def authorizeMySpaceAccess
+    
+  end
   
+  def retrieveMySpaceContacts
+    
+  end
+  
+private
+
 end
