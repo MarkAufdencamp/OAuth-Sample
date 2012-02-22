fb71c1294b8b71bdbecc6495d60fe5eacf81f91d
student <student@lappy.(none)>
2012-02-23 07:10:43 +0800

    Added Resource based User Model. Migration, Controller, Views

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120220041934) do
+ActiveRecord::Schema.define(:version => 20120222201224) do
 
   create_table "sessions", :force => true do |t|
     t.string   "session_id", :null => false
@@ -20,5 +20,15 @@ ActiveRecord::Schema.define(:version => 20120220041934) do
 
   add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
   add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
+
+  create_table "users", :force => true do |t|
+    t.string   "username"
+    t.string   "email"
+    t.string   "crypted_password"
+    t.string   "password_salt"
+    t.string   "persistence_token"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
