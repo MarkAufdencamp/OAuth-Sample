6d999dc11a6281c634841c6f0eac7a2ade8fff0b
Mark Aufdencamp <Mark@Aufdencamp.com>
2012-02-23 21:36:19 -0500

    Added Login/Logout Routes to User_Session

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 ActionController::Routing::Routes.draw do |map|
+
+  map.login "login", :controller => "user_sessions", :action = > "new"
+  map logout "logout", :controller => "user_sessions", :action => "destroy"
+
   map.resources :user_sessions
 
   map.resources :users
